fd75bc61c516700a529e560d30e452f24409b744
neilAnaya <120688501+neilAnaya@users.noreply.github.com>
2023-11-11 18:30:57 -0500

    agrega eliminar
    eliminar en el controller

--- TDB-main/MsSeguridad/src/main/java/TDB/MsSeguridad/controller/AuthController.java
+++ TDB-main/MsSeguridad/src/main/java/TDB/MsSeguridad/controller/AuthController.java
@@ -3,13 +3,16 @@ package TDB.MsSeguridad.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import TDB.MsSeguridad.services.AuthService;
-
 import TDB.MsSeguridad.model.UsuarioModel;
+import TDB.MsSeguridad.services.AuthService;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -22,4 +25,11 @@ public class AuthController {
     public List<UsuarioModel> getAll() {
         return authService.getAll();
     }
+
+        @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<UsuarioModel> deleteById(@PathVariable int id){
+         authService.deleteById(id);
+         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
